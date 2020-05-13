33e4a61998f99b2953e4984502cbe0b35da6158a
ataykaylar <57875291+ataykaylar@users.noreply.github.com>
2020-05-13 23:38:24 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "SimpleReg.h"
+#include "SimpleTeam.h"
+using namespace std;
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    SimpleReg reg;
+    reg.addTeam("Team 1","Red", 2020);
+    reg.removeTeam("TEAM 1");
+
+
+
+
+
+    reg.displayAllTeams();
+}
